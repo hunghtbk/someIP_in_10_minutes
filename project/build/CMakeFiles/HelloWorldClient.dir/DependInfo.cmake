917d2caee160aeb7c2572e9e9d06f86d5ea533a5
hung.hoang <hunght.thaibinh@gmail.com>
2020-02-24 01:22:33 -0800

    source

---
+++ project/build/CMakeFiles/HelloWorldClient.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hunght/Study/project/src-gen/v1/commonapi/HelloWorldSomeIPDeployment.cpp" "/home/hunght/Study/project/build/CMakeFiles/HelloWorldClient.dir/src-gen/v1/commonapi/HelloWorldSomeIPDeployment.cpp.o"
+  "/home/hunght/Study/project/src-gen/v1/commonapi/HelloWorldSomeIPProxy.cpp" "/home/hunght/Study/project/build/CMakeFiles/HelloWorldClient.dir/src-gen/v1/commonapi/HelloWorldSomeIPProxy.cpp.o"
+  "/home/hunght/Study/project/src/HelloWorldClient.cpp" "/home/hunght/Study/project/build/CMakeFiles/HelloWorldClient.dir/src/HelloWorldClient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././src-gen"
+  "../../someip/capicxx-core-runtime/include"
+  "../../someip/capicxx-someip-runtime/include"
+  "../../someip/vSomeIP/interface"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
